a928c6cb85033ae62b4bd5fc8596aa25dffc251c
alexishooks <alexishooks207@gmail.com>
2022-11-01 16:47:26 -0400

    fixed?

--- src/tuiter/NavigationSidebar/index.js
+++ src/tuiter/NavigationSidebar/index.js
@@ -8,36 +8,36 @@ const NavigationSidebar = (
 ) => {
   return (
       <div className="list-group">
-        <a href="#" className="list-group-item">Tuiter</a>
-        <a href="#" className={`list-group-item
+        <a href="#/" className="list-group-item">Tuiter</a>
+        <a href="#/" className={`list-group-item
                     ${active === 'home'?'active':''}`}>
           <div className="d-none d-lg-block">Home</div>
         </a>
-        <a href="#" className={`list-group-item d-none d-lg-block
+        <a href="#/" className={`list-group-item d-none d-lg-block
                     ${active === 'explore'?'active':''}`}>
           <div className="d-none d-lg-block">Explore</div>
         </a>
-        <a href="#" className={`list-group-item d-none d-lg-block
+        <a href="#/" className={`list-group-item d-none d-lg-block
                     ${active === 'notifications'?'active':''}`}>
           <div className="d-none d-lg-block">Notifications</div>
         </a>
-        <a href="#" className={`list-group-item 
+        <a href="#/" className={`list-group-item 
                     ${active === 'messages'?'active':''}`}>
           <div className="d-none d-lg-block">Messages</div>
         </a>
-        <a href="#" className={`list-group-item
+        <a href="#/" className={`list-group-item
                     ${active === 'bookmarks'?'active':''}`}>
           <div className="d-none d-lg-block">Bookmarks</div>
         </a>
-        <a href="#" className={`list-group-item
+        <a href="#/" className={`list-group-item
                     ${active === 'lists'?'active':''}`}>
           <div className="d-none d-lg-block">Lists</div>
         </a>
-        <a href="#" className={`list-group-item
+        <a href="#/" className={`list-group-item
                     ${active === 'profile'?'active':''}`}>
           <div className="d-none d-lg-block">Profile</div>
         </a>
-        <a href="#" className={`list-group-item
+        <a href="#/" className={`list-group-item
                     ${active === 'more'?'active':''}`}>
           <div className="d-none d-lg-block">More</div>
         </a>
