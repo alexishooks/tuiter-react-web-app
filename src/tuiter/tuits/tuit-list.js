d0626138458355b5072f1ed5564f909dcaeb105f
alexishooks <alexishooks207@gmail.com>
2022-11-20 22:22:40 -0500

    k

--- src/tuiter/tuits/tuit-list.js
+++ src/tuiter/tuits/tuit-list.js
@@ -11,7 +11,7 @@ const TuitList = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(findTuitsThunk())
-  }, [])
+  }, [dispatch])
   return(
       <ul className="list-group">
         {
